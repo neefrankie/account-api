a5d9c2370b1ea6e66d281214bd31eeb130a2dd8e
neefrankie <neefrankie@gmail.com>
2022-01-18 20:59:41 +0800

    initial

---
+++ src/main/java/com/github/neefrankie/App.java
@@ -0,0 +1,14 @@
+package com.github.neefrankie;
+
+import io.javalin.Javalin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        Javalin app = Javalin.create().start(7070);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+}
